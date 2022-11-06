bb719b2bb142a343f5bd4fc10d458f15da9f4f45
loufi23 <grabouille650@gmail.com>
2022-11-06 14:47:34 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Découvrez Google&nbsp;Keep – Enregistrez vos pensées, où que vous soyez – Keep – Google</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg position-fixed w-100">
+      <div class="container-fluid">
+        <a class="navbar-brand py-3 fw-bolder">Google Keep</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link"><i class="bi-facebook"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"><i class="bi-twitter"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <section class="banner d-flex justify-content-center align-items-center pt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1>
+              Pour ne rien oublier,<br> où que vous soyez
+            </h1>
+            <p class="button pt-5">
+              <button class="btn btn-primary" onclick="myFunction()">Essayer Google Keep</button>
+              <!-- <div class="btn-group">
+                <a href="#" class="btn btn-primary">Android</a>
+                <a href="#" class="btn btn-primary">IOS</a>
+                <a href="#" class="btn btn-primary">Web Version</a>
+              </div> -->
+            </p>
+          </div>
+          <div class="col-md-6"></div>
+        </div>
+      </div>
+    </section>
+    <section class="notes pb-5">
+      <div class="container pt-5">
+        <div class="row ">
+        <h2 class=" text-center">Notez tout ce qui vous passe par la tête</h2>
+        <p class=" text-center pt-3 "> Ajoutez des notes, des listes, des photos et des contenus audio à Keep.</p>
+        <div class="row justify-content-evenly pt-3">
+          <div class="col-4">
+            <div class="cristel">
+              <h2 class="fete">Fête surprise de Christelle</h2>
+              <hr class="dotted">
+              <p class="">Rappel à 20:00 le 19 juin</p>
+            </div>
+            <div class="text-center">
+              <i class="bi-book"></i>
+            </div>
+          </div>
+          <div class=" col-4">
+            <div class="liste">
+              <h4> Idées cadeau</h4>
+              <hr class="">
+              <hr class="">
+              <ul class="">
+                <li class="">Nouveau casque de vélo</li>
+                <li class="">Bougies ?</li>
+                <li class="">Jolie plante verte ?</li>
+                <li class="">Boucles d'oreilles</li>
+                <p class="">...</p>
+              </ul>
+            </div>
+            <div class="text-center">
+              <i class="bi-list"></i>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-evenly pt-5">
+          <div class="col-4">
+            <div class="image">
+              <img src="../images/fleur.jpg" style="height: 250px; width: 288px; opacity:0.5;">
+              <p class="text-center">Backup gift idea</p>
+            </div>
+            <div class="text-center">
+              <i class="bi-camera"></i>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="titre">
+              <h4> Titre</h4>
+                <ul class="">Nettoyer les tables de pique-niques</ul>
+              <br> <br><br><br><br><br><br>
+              <p><i class="bi-clock"></i> Après-midi</p>
+            </div>
+            <div class="text-center">
+              <i class="bi-mic"></i>
+            </div>
+          </div>
+        </div>
+
+        </div>
+      </div>
+
+    </section >
+   <div class="container text-center pt-5"> 
+    <div class="col-md-12 ">
+      <hr class="">
+    </div>
+   </div>
+    <section class="tel">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 offset-md-2 align-self-center">
+            <h2>Votre note s'affiche quand vous en avez besoin</h2>
+            <p>Il vous manque des ingrédients pour ce soir ? Définissez un rappel basé sur le lieu pour afficher votre liste de courses en arrivant au magasin. Vous avez une liste de tâches à terminer ? Définissez un rappel basé sur l'heure pour ne jamais rien oublier.</p>
+          </div>
+          <div class="col-md-3 ">
+            <img src="../images/te.png" style="height: 500px; width: 400px;">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="paralax d-flex justify-content-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2 me-auto">
+            <img src="../images/main.png" style="height:500px; width:400px;">
+          </div>
+          <div class="col-md-4 ms-md-auto pt-5"style="color:white;">
+            <h2>Partagez vos pensées avec vos proches</h2>
+            <p>
+             Si vous êtes plusieurs à faire les courses, partagez votre liste sur Keep. Chacun peut cocher les articles qu'il achète en temps réel. Plus besoin d'échanger des SMS. À plusieurs, tout va plus vite !
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="tel2">
+      <div class="container pt-5">
+        <div class="row justify-content-evenly">
+          <div class="col-md-4 offset-md-1 align-self-center">
+            <h2> Retrouvez vos notes en un clin d'œil</h2>
+            <p>  Filtrez et recherchez rapidement des notes par couleur et en fonction d'autres attributs (listes contenant des images, notes audio accompagnées de rappels ou notes partagées, par exemple). Trouvez plus rapidement ce que vous cherchez, et laissez Keep faire travailler sa mémoire.</p>
+          </div>
+          <div class="col-md-3 ">
+            <img src="../images/tel.png" style="height: 500px; width: 300px;">
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="container text-center pt-5"> 
+      <div class="col-md-12 ">
+        <hr class="">
+      </div>
+   </div>
+   <section id="ordi">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-2 align-self-center">
+          <img src="../images/telkeep2.png" style="height: 250px;width:200px;">
+        </div>
+        <div class="col-md-6 offset-md-1 justify-content-center">
+          <img src="../images/ordi1.png" style="height: 400px;width:400px;">
+        </div>
+        <div class="col-md-2 align-self-center ">
+          <img src="../images/telkeep.png" style="height: 250px;width:200px;">
+        </div>
+        <div class="col-md-8 offset-md-2">
+          <h2>Toujours à portée de main</h2>
+            <p>Keep fonctionne sur votre téléphone, votre tablette et votre ordinateur. Toutes vos notes sont synchronisées sur tous vos appareils et vous suivent partout.</p>
+            <p style="color:#8a8a8a;">Une connexion Internet est requise pour la synchronisation sur les appareils.</p>
+        </div>
+      </div>
+    </div>
+   </section>
+   <section class="banner2 pt-5 ">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-10 offset-md-1 pt-5">
+          <h1> Ne perdez plus le fil de vos pensées</h1>
+          <p class="button pt-5">
+            <button class="btn btn-primary">Essayer Google Keep</button>
+          </p>
+        </div>
+      </div>
+    </div>
+   </section>
+   <section class="footer pt-5">
+    <div class="container pt-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h1></h1>
+          <p></p>
+        </div>
+      </div>
+    </div>
+   </section>
+    <div class="foot">
+      <div class="pt-2">
+        <ul>
+          <li><a href="#">Google</a></li>
+          <li><a href="#">À propos de Google</a></li>
+          <li><a href="#">Confidentialité</a></li>
+          <li><a href="#">Conditions</a></li>
+        </ul>
+      </div>
+    </div>
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
