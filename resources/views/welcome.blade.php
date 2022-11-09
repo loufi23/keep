56c8b0d26b16875ff45ea1179c8859aabfe0707a
loufi23 <grabouille650@gmail.com>
2022-11-09 16:30:27 +0000

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Découvrez Google&nbsp;Keep – Enregistrez vos pensées, où que vous soyez – Keep – Google</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Recursive:wght@400;700&display=swap">
+    <link href=
+      '//fonts.googleapis.com/css?family=Open+Sans:300,400,600|Sacramento|Shadows+Into+Light+Two|Roboto+Slab:100,300,400&amp;subset=latin,latin-ext'
+      rel='stylesheet' type='text/css' nonce="Nsl-4sJEzd2CHrs86DS6MA">
+      <link href=
+      "//fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700&amp;subset=latin,latin-ext"
+      rel="stylesheet" nonce="Nsl-4sJEzd2CHrs86DS6MA">
+      <link href=
+      "//fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&amp;subset=latin,latin-ext"
+      rel="stylesheet" nonce="Nsl-4sJEzd2CHrs86DS6MA">
+      <link href="//fonts.googleapis.com/css?family=Product+Sans" rel="stylesheet" nonce="Nsl-4sJEzd2CHrs86DS6MA">
+      <link href="css/style.css" rel="stylesheet">
+
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg position-fixed w-100">
+    <nav class="navbar navbar-expand-lg position-fixed w-100" id="navbar">
       <div class="container-fluid">
-        <a class="navbar-brand py-3 fw-bolder">Google Keep</a>
+        <a class="navbar-brand py-3 fw-bolder" >Google Keep</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -24,9 +36,11 @@
               <a class="nav-link"><i class="bi-twitter"></i></a>
             </li>
           </ul>
+          <button class="btn">Télécharger Keep</button>
         </div>
       </div>
     </nav>
+   
     <section class="banner d-flex justify-content-center align-items-center pt-5">
       <div class="container">
         <div class="row">
@@ -35,7 +49,7 @@
               Pour ne rien oublier,<br> où que vous soyez
             </h1>
             <p class="button pt-5">
-              <button class="btn btn-primary" onclick="myFunction()">Essayer Google Keep</button>
+              <button class="btn btn-primary" >Essayer Google Keep</button>
               <!-- <div class="btn-group">
                 <a href="#" class="btn btn-primary">Android</a>
                 <a href="#" class="btn btn-primary">IOS</a>
@@ -49,79 +63,131 @@
     </section>
     <section class="notes pb-5">
       <div class="container pt-5">
-        <div class="row ">
-        <h2 class=" text-center">Notez tout ce qui vous passe par la tête</h2>
-        <p class=" text-center pt-3 "> Ajoutez des notes, des listes, des photos et des contenus audio à Keep.</p>
-        <div class="row justify-content-evenly pt-3">
-          <div class="col-4">
-            <div class="cristel">
-              <h2 class="fete">Fête surprise de Christelle</h2>
-              <hr class="dotted">
-              <p class="">Rappel à 20:00 le 19 juin</p>
+          <h2 class=" text-center">Notez tout ce qui vous passe par la tête</h2>
+          <p class=" text-center pt-3 "> Ajoutez des notes, des listes, des photos et des contenus audio à Keep.</p>
+          <div class="row justify-content-evenly pt-3">
+            <div class="col-4">
+              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="cristel">
+                      <h3 class="fete">Fête surprise de Christelle</h3>
+                      <hr class="dot">
+                      <p class="mini"><i class="bi-clock"></i> Rappel à 20:00 le 19 juin</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="papier">
+                      <h3 class="fete2 text-center">Fête <br> surprise de <br> Christelle</h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="text-center pt-2">
+                  <a class="bi-book" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next"></a>
+                </div>
+              </div>
             </div>
-            <div class="text-center">
-              <i class="bi-book"></i>
-            </div>
-          </div>
           <div class=" col-4">
-            <div class="liste">
-              <h4> Idées cadeau</h4>
-              <hr class="">
-              <hr class="">
-              <ul class="">
-                <li class="">Nouveau casque de vélo</li>
-                <li class="">Bougies ?</li>
-                <li class="">Jolie plante verte ?</li>
-                <li class="">Boucles d'oreilles</li>
-                <p class="">...</p>
-              </ul>
-            </div>
-            <div class="text-center">
-              <i class="bi-list"></i>
-            </div>
+              <div id="carouselExampleControls1" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="liste">
+                      <h4> Idées cadeau</h4>
+                      <ul class="">
+                        <li class="">Nouveau casque de vélo</li>
+                        <li class="">Bougies ?</li>
+                        <li class="">Jolie plante verte ?</li>
+                        <li class="">Boucles d'oreilles</li>
+                        <p class="">...</p>
+                      </ul>
+                      <hr class="dot">
+                      <p class="mini"> <i class="bi-map"></i> Rappel du grand marché</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="liste2">
+                      <h3 class="text-center"> Idées cadeau</h3>
+                      <p>Nouveau casque de vélo</p>
+                      <p>Bougies ?</p>
+                      <p>Jolie plante verte ?</p>
+                      <p>Boucles d'oreilles</p>
+                      <p class="element">Bracelets</p>
+                      <p class="element1">Écharpe ou pashmina ?</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="text-center pt-2">
+                  <a class="bi-list" type="button" data-bs-target="#carouselExampleControls1" data-bs-slide="next"></a>
+                </div>
+              </div>
           </div>
         </div>
         <div class="row justify-content-evenly pt-5">
           <div class="col-4">
-            <div class="image">
-              <img src="../images/fleur.jpg" style="height: 250px; width: 288px; opacity:0.5;">
-              <p class="text-center">Backup gift idea</p>
-            </div>
-            <div class="text-center">
-              <i class="bi-camera"></i>
+            <div id="carouselExampleControls2" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="image">
+                    <img src="../images/photo2.jpg" style="height: 240px; width: 297px;">
+                    <p class="text-center">Backup gift idea</p>
+                   </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="image2">
+                    <img src="../images/photo.jpg" style="height: 270px; width: 297px;opacity:0.6; background-color:orange;">
+                  </div>
+                </div>
+              </div>
+              <div class="text-center pt-2">
+                <a class="bi-camera" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next"></a>
+              </div>
             </div>
           </div>
-          <div class="col-4">
-            <div class="titre">
-              <h4> Titre</h4>
-                <ul class="">Nettoyer les tables de pique-niques</ul>
-              <br> <br><br><br><br><br><br>
-              <p><i class="bi-clock"></i> Après-midi</p>
-            </div>
-            <div class="text-center">
-              <i class="bi-mic"></i>
+          <div class="col-4"> 
+            <div id="carouselExampleControls4" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="titre">
+                    <h4> Titre</h4>
+                    <ul class="">Nettoyer les tables de pique-niques</ul>
+                    <br><br><br> <br>
+                    <hr class="dot">
+                    <p class="mini"><i class="bi-clock"></i> Après-midi</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="titre2">
+                    <h4 class="text-center pt-2">Parlez !</h4>
+                    <div class="pt-5">
+                      <a class="pulse-primary  ">
+                        <i class="bi-mic "></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="text-center pt-2">
+                <a class="bi-mic" type="button" data-bs-target="#carouselExampleControls4" data-bs-slide="next"></a>
+              </div>
             </div>
           </div>
-        </div>
-
         </div>
       </div>
-
     </section >
-   <div class="container text-center pt-5"> 
-    <div class="col-md-12 ">
-      <hr class="">
+    <div class="container text-center pt-5"> 
+      <div class="col-md-12 ">
+        <hr class="">
+      </div>
     </div>
-   </div>
-    <section class="tel">
-      <div class="container">
+    <section class="tel pt-5">
+      <div class="container pt-5">
         <div class="row">
           <div class="col-md-4 offset-md-2 align-self-center">
             <h2>Votre note s'affiche quand vous en avez besoin</h2>
             <p>Il vous manque des ingrédients pour ce soir ? Définissez un rappel basé sur le lieu pour afficher votre liste de courses en arrivant au magasin. Vous avez une liste de tâches à terminer ? Définissez un rappel basé sur l'heure pour ne jamais rien oublier.</p>
           </div>
           <div class="col-md-3 ">
-            <img src="../images/te.png" style="height: 500px; width: 400px;">
+            <img src="../images/te.png" style="height: 600px; width: 400px;">
           </div>
         </div>
       </div>
@@ -130,13 +196,11 @@
       <div class="container">
         <div class="row">
           <div class="col-md-2 me-auto">
-            <img src="../images/main.png" style="height:500px; width:400px;">
+            <img id="main" class="main" src="../images/hand.png" style="height:500px; width:400px;">
           </div>
           <div class="col-md-4 ms-md-auto pt-5"style="color:white;">
-            <h2>Partagez vos pensées avec vos proches</h2>
-            <p>
-             Si vous êtes plusieurs à faire les courses, partagez votre liste sur Keep. Chacun peut cocher les articles qu'il achète en temps réel. Plus besoin d'échanger des SMS. À plusieurs, tout va plus vite !
-            </p>
+            <h3>Partagez vos pensées avec vos proches</h3>
+            <p>Si vous êtes plusieurs à faire les courses, partagez votre liste sur Keep. Chacun peut cocher les articles qu'il achète en temps réel. Plus besoin d'échanger des SMS. À plusieurs, tout va plus vite !</p>
           </div>
         </div>
       </div>
@@ -149,58 +213,59 @@
             <p>  Filtrez et recherchez rapidement des notes par couleur et en fonction d'autres attributs (listes contenant des images, notes audio accompagnées de rappels ou notes partagées, par exemple). Trouvez plus rapidement ce que vous cherchez, et laissez Keep faire travailler sa mémoire.</p>
           </div>
           <div class="col-md-3 ">
-            <img src="../images/tel.png" style="height: 500px; width: 300px;">
+            <img src="../images/nexus.jpg" style="height: 600px; width: 400px;">
           </div>
         </div>
       </div>
     </section>
-    <div class="container text-center pt-5"> 
-      <div class="col-md-12 ">
-        <hr class="">
+    <div class="space"></div>
+    <section class="ordi ">
+      <div class="container text-center pt-0"> 
+        <div class=" col-md-12 ">
+          <hr class=" ">
+        </div>
       </div>
-   </div>
-   <section id="ordi">
-    <div class="container">
-      <div class="row text-center">
-        <div class="col-md-2 align-self-center">
-          <img src="../images/telkeep2.png" style="height: 250px;width:200px;">
-        </div>
-        <div class="col-md-6 offset-md-1 justify-content-center">
-          <img src="../images/ordi1.png" style="height: 400px;width:400px;">
-        </div>
-        <div class="col-md-2 align-self-center ">
-          <img src="../images/telkeep.png" style="height: 250px;width:200px;">
-        </div>
-        <div class="col-md-8 offset-md-2">
-          <h2>Toujours à portée de main</h2>
+      <div class="container pt-5">
+        <div class="row text-center">
+          <div class="col-md-2 align-self-center">
+            <img id="tel1" src="../images/keep1.png" style="height: 200px;width:100px;">
+          </div>
+          <div class="col-md-6 offset-md-1 justify-content-center">
+            <img src="../images/chromebook.jpg" style="height: 200px;width:400px;">
+          </div>
+          <div class="col-md-2 align-self-center ">
+            <img id="tel2" src="../images/keep.png" style="height: 250px;width:200px;">
+          </div>
+          <div class="col-md-8 offset-md-2">
+            <h2>Toujours à portée de main</h2>
             <p>Keep fonctionne sur votre téléphone, votre tablette et votre ordinateur. Toutes vos notes sont synchronisées sur tous vos appareils et vous suivent partout.</p>
             <p style="color:#8a8a8a;">Une connexion Internet est requise pour la synchronisation sur les appareils.</p>
+          </div>
         </div>
       </div>
-    </div>
-   </section>
-   <section class="banner2 pt-5 ">
-    <div class="container">
-      <div class="row text-center">
-        <div class="col-md-10 offset-md-1 pt-5">
-          <h1> Ne perdez plus le fil de vos pensées</h1>
-          <p class="button pt-5">
-            <button class="btn btn-primary">Essayer Google Keep</button>
-          </p>
+    </section>
+    <section class="banner2 pt-5 ">
+      <div class="container pt-5">
+        <div class="row text-center">
+          <div class="col-md-10 offset-md-1 pt-5">
+            <h1> Ne perdez plus le fil de vos pensées</h1>
+            <p class="button pt-5">
+              <button class="btn btn-primary">Essayer Google Keep</button>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-   </section>
-   <section class="footer pt-5">
-    <div class="container pt-5">
-      <div class="row">
-        <div class="col-md-12">
-          <h1></h1>
-          <p></p>
+    </section>
+    <section class="footer pt-5">
+      <div class="container pt-5">
+        <div class="row">
+          <div class="col-md-12">
+            <h1></h1>
+            <p></p>
+          </div>
         </div>
       </div>
-    </div>
-   </section>
+    </section>
     <div class="foot">
       <div class="pt-2">
         <ul>
@@ -212,5 +277,33 @@
       </div>
     </div>
     <script src="js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script>
+      window.addEventListener("scroll", function() {
+        var nav = document.querySelector("nav");
+        nav.classList.toggle("sticky",window.scrollY > 480);
+      })
+      var main= document.getElementById('main')
+      scroll = window.pageYOffset;
+      document.addEventListener('scroll',function(e){
+        var offset= window.pageYOffset;
+        scroll= offset;
+        main.style.width= (100 + scroll/12) +'%';
+      });
+      var tel1= document.getElementById('tel1')
+      scroll = window.pageYOffset;
+      document.addEventListener('scroll',function(e){
+        var offset= window.pageYOffset;
+        scroll= offset;
+        tel1.style.width= (100 + scroll/100) +'%';
+      });
+      var tel2= document.getElementById('tel2')
+      scroll = window.pageYOffset;
+      document.addEventListener('scroll',function(e){
+        var offset= window.pageYOffset;
+        scroll= offset;
+        tel2.style.width= (100 + scroll/90) +'%';
+        tel2.style.left= (100 + scroll/90) +'%';
+      });
+    </script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/scroll', function () {
+    return view('oncrolll');
+});
